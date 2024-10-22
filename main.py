7a82c1018c21818d995c8c74c28e49a1f098df44
begerowiec <agadrianekk16@gmail.com>
2024-10-23 01:57:37 +0200

    Create endpoint POST /cars

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException, Depends, status
 from pydantic import BaseModel
 from typing import Annotated
 import models
+import schemas
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 
@@ -14,9 +15,20 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 # Dependency to get a database session
+
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+
+
+@app.post("/cars/", response_model=schemas.Car)
+def create_car(car: schemas.CarCreate, db: Session = Depends(get_db)):
+    db_car = models.Car(**car.dict())
+    db.add(db_car)
+    db.commit()
+    db.refresh(db_car)
+    return db_car
