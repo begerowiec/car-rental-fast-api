7fe2f85bce0363f23a36418d16a83448ad958e82
begerowiec <agadrianekk16@gmail.com>
2024-10-23 21:09:49 +0200

    Remove unused imports, add GET endpoints for all cars and single car
    Changes:
    -Removed unnecessary imports to clean up the codebase.
    -Added new GET endpoints:
    *GET /cars: Fetches all cars from the database.
    *GET /cars/{id}: Fetches a specific car by its ID.

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from fastapi import FastAPI, HTTPException, Depends, status
-from pydantic import BaseModel
-from typing import Annotated
+from fastapi import FastAPI, Depends, HTTPException
 import models
 import schemas
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from typing import List
 
 
 app = FastAPI()
@@ -25,6 +24,10 @@ def get_db():
         db.close()
 
 
+# ---------------------------
+# Car Endpoints
+# ---------------------------
+
 @app.post("/cars/", response_model=schemas.Car)
 def create_car(car: schemas.CarCreate, db: Session = Depends(get_db)):
     db_car = models.Car(**car.dict())
@@ -32,3 +35,17 @@ def create_car(car: schemas.CarCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(db_car)
     return db_car
+
+
+@app.get("/cars/", response_model=List[schemas.Car])
+def read_cars(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    cars = db.query(models.Car).offset(skip).limit(limit).all()
+    return cars
+
+
+@app.get("/cars/{car_id}", response_model=schemas.Car)
+def read_car(car_id: int, db: Session = Depends(get_db)):
+    car = db.query(models.Car).filter(models.Car.id == car_id).first()
+    if car is None:
+        raise HTTPException(status_code=404, detail="Car not found")
+    return car
